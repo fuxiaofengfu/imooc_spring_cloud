dfd1e34d293109be7ef9f6ebb0814857117f9d84
xiaofengfu <fuxiaofeng1107@163.com>
2019-12-22 17:17:45 +0800

    init project

--- spring-cloud-client/src/main/java/com/cloud/client/Client2Application.java
+++ spring-cloud-client/src/main/java/com/cloud/client/Client2Application.java
@@ -4,15 +4,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Profile;
-import org.springframework.core.env.ConfigurableEnvironment;
 
 @EnableEurekaClient
 @SpringBootApplication
-public class ClientRouteTraceApplication {
+public class Client2Application {
 
 	public static void main(String[] args) {
-		SpringApplication springApplication = new SpringApplication(ClientRouteTraceApplication.class);
-		springApplication.setAdditionalProfiles("client1");
+		SpringApplication springApplication = new SpringApplication(Client2Application.class);
+		springApplication.setAdditionalProfiles("client2");
 		springApplication.run(args);
 	}
 }
--- spring-cloud-client/src/main/java/com/cloud/client/controller/DemoController.java
+++ spring-cloud-client/src/main/java/com/cloud/client/controller/DemoController.java
@@ -1,8 +1,5 @@
 package com.cloud.client.controller;
 
-import org.springframework.cloud.sleuth.annotation.ContinueSpan;
-import org.springframework.cloud.sleuth.annotation.NewSpan;
-import org.springframework.cloud.sleuth.annotation.SpanTag;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -11,10 +8,18 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class DemoController {
 
-	@RequestMapping(value={"demo"},method = {RequestMethod.POST,RequestMethod.GET})
-	public String demoTest(@RequestParam String test){
-		if("e".equals(test)){
-			throw new RuntimeException("你犯错了。。。。");
+	@RequestMapping(value = {"client1"}, method = {RequestMethod.POST, RequestMethod.GET})
+	public String client1(@RequestParam String test) {
+		if ("e".equals(test)) {
+			throw new RuntimeException("client1 param test is error。。。。");
+		}
+		return test;
+	}
+
+	@RequestMapping(value = {"client2"}, method = {RequestMethod.POST, RequestMethod.GET})
+	public String client2(@RequestParam String test) {
+		if ("e".equals(test)) {
+			throw new RuntimeException("client2 param test is error。。。。");
 		}
 		return test;
 	}
