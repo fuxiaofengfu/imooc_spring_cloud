6d4ecb481d27b56014ca780c5bc23049647b0260
xiaofengfu <fuxiaofeng1107@163.com>
2019-12-27 21:46:32 +0800

    init project

--- spring-cloud-zuul/src/main/java/com/cloud/zuul/filter/ErrorFilterDemo.java
+++ spring-cloud-zuul/src/main/java/com/cloud/zuul/filter/ErrorFilterDemo.java
@@ -1,33 +1,34 @@
 package com.cloud.zuul.filter;
 
-import com.netflix.zuul.ZuulFilter;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ObjectNode;
 import com.netflix.zuul.context.RequestContext;
-import com.netflix.zuul.exception.ZuulException;
-import org.springframework.cloud.netflix.zuul.filters.support.FilterConstants;
+import org.springframework.cloud.netflix.zuul.filters.post.SendErrorFilter;
+import org.springframework.util.ReflectionUtils;
+import org.springframework.util.StringUtils;
 
-public class ErrorFilterDemo extends ZuulFilter {
-	@Override
-	public String filterType() {
-		return FilterConstants.ERROR_TYPE;
-	}
+public class ErrorFilterDemo extends SendErrorFilter {
 
 	@Override
-	public int filterOrder() {
-		return FilterConstants.SEND_ERROR_FILTER_ORDER - 1;
-	}
-
-	@Override
-	public boolean shouldFilter() {
-		RequestContext ctx = RequestContext.getCurrentContext();
-		return ctx.getThrowable() != null;
-	}
-
-	@Override
-	public Object run() throws ZuulException {
-		RequestContext currentContext = RequestContext.getCurrentContext();
-		Throwable throwable = currentContext.getThrowable();
-//		currentContext.set("error.status_code", HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
-//		currentContext.set("error.exception", throwable.getCause());
+	public Object run() {
+		try {
+			RequestContext ctx = RequestContext.getCurrentContext();
+			ExceptionHolder exception = findZuulException(ctx.getThrowable());
+			ObjectMapper objectMapper = new ObjectMapper();
+			ObjectNode objectNode = objectMapper.createObjectNode();
+			objectNode.put("error.status_code", exception.getStatusCode());
+			objectNode.put("error.exception", exception.getThrowable().getMessage());
+			if (StringUtils.hasText(exception.getErrorCause())) {
+				objectNode.put("error.cause", exception.getErrorCause());
+			}
+			if (!ctx.getResponse().isCommitted()) {
+				ctx.getResponse().getOutputStream().print(objectNode.toString());
+				ctx.setResponseStatusCode(exception.getStatusCode());
+			}
+		} catch (Exception ex) {
+			ReflectionUtils.rethrowRuntimeException(ex);
+		}
 		return null;
 	}
+
 }
